cd583f2d9a7c4cc9fbba3f3be71bb304c994e638
Simpag <simon.granstrom@gmail.com>
2024-05-20 20:21:19 +0200

    initial diffusion model

---
+++ DiffusionModel/DiffusionModel.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+import wandb
+import os
+
+class DiffusionModel(nn.Module):
+    def __init__(self) -> None:
+        pass
+    
+    def sample(self):
+        pass
+
+    def load_model(self):
+        pass
+
+    def save_model(self):
+        pass
