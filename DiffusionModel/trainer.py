69d39e379706bfc9b06785a1aa5b1af6de289fb9
Simpag <simon.granstrom@gmail.com>
2024-05-21 22:30:06 +0200

    added check for image size

--- DiffusionModel/trainer.py
+++ DiffusionModel/trainer.py
@@ -90,6 +90,8 @@ class Trainer:
         real_images_sample = np.random.choice(len(self.test_data.data), len(sampled_images))
         real_images_sample = self.test_data.data[real_images_sample, :, :, :]
         real_images_sample = torch.from_numpy(real_images_sample).permute((0,3,1,2)).to(self.device)
+        assert real_images_sample.shape == (len(sampled_images), self.img_channels, self.img_size, self.img_size), f"Wrong image size from dataset, should be {(len(sampled_images), self.img_channels, self.img_size, self.img_size)}, got {real_images_sample.shape}"
+
         fid_score, is_score, is_deviation = evaluate_generator(generated_images=sampled_images, real_images=real_images_sample, normalized_images=False)
         log_dict = {
                     "val_mse": avg_loss,
